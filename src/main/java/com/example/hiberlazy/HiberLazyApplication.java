1f59bfe84bcb07376a3c00efd8fe5c1c3940494a
markoff-s <markov.slava@gmail.com>
2018-03-29 14:40:19 -0400

    project

---
+++ src/main/java/com/example/hiberlazy/HiberLazyApplication.java
@@ -0,0 +1,12 @@
+package com.example.hiberlazy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiberLazyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HiberLazyApplication.class, args);
+	}
+}
